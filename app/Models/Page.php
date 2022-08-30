3922b10106772dabfd032c657d3db4580c7d218b
Mahbub Habib <mahbub.habib10@gmail.com>
2022-08-30 15:00:39 +0600

    update page

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -39,40 +39,4 @@ class Page extends Model
     {
         return $this->hasMany(Page::class, 'parent_id');
     }
-
-    public function newNestedSetQuery($excludeDeleted = true) {
-        $builder = $this->newQuery($excludeDeleted)->orderBy($this->getQualifiedOrderColumnName());
-
-        if ( $this->isScoped() ) {
-          foreach($this->scoped as $scopeFld)
-            $builder->where($scopeFld, '=', $this->$scopeFld);
-        }
-
-        return $builder;
-      }
-
-
-    public function getAncestorsAndSelf($columns = array('*')) {
-        return $this->ancestorsAndSelf()->get($columns);
-      }
-
-      public function ancestorsAndSelf() {
-        return $this->newNestedSetQuery()
-                    ->where('slug', '<=', $this->getAttribute('slug'))
-                    ->where('slug', '>=', $this->getAttribute('slug'));
-      }
-
-      public function getQualifiedOrderColumnName() {
-        return $this->getTable() . '.' . $this->getOrderColumnName();
-      }
-
-      public function getOrderColumnName() {
-        return is_null($this->orderColumn) ? 'slug' : null;
-      }
-      public function isScoped() {
-        return !!(count($this->getScopedColumns()) > 0);
-      }
-      public function getScopedColumns() {
-        return (array) $this->scoped;
-      }
 }

1f52490a122048c9a63e4497bc1b55cddfabdb96
Mahbub Habib <mahbub.habib10@gmail.com>
2022-08-30 12:23:56 +0600

    app view updated

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>@yield('title')</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.3/toastr.css" integrity="sha512-oe8OpYjBaDWPt2VmSFR+qYOdnTjeV9QPLJUeqZyprDEQvQLJ9C5PCFclxwNuvb/GQgQngdCXzKSFltuHD3eCxA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         @yield('styles')
     </head>
 
@@ -16,6 +17,27 @@
         @yield('content')
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js" ></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.1.3/toastr.min.js" integrity="sha512-lbwH47l/tPXJYG9AcFNoJaTMhGvYWhVM9YI43CT+uteTRRaiLCui8snIgyAN8XWgNjNhCqlAUdzZptso6OCoFQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script>
+            @if(Session::has('message'))
+               var type = "{{ Session::get('alert-type', 'info') }}";
+               switch(type){
+                   case 'info':
+                       toastr.info("{{ Session::get('message') }}");
+                       break;
+                   case 'warning':
+                       toastr.warning("{{ Session::get('message') }}");
+                       break;
+                   case 'success':
+                       toastr.success("{{ Session::get('message') }}");
+                       break;
+                   case 'error':
+                       toastr.error("{{ Session::get('message') }}");
+                       break;
+               }
+             @endif
+       </script>
         @stack('scripts')
     </body>
 
